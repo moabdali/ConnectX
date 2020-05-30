50e6eb4af9579c0197ba571a085d8ed562980297
moabdali <64987634+moabdali@users.noreply.github.com>
2020-05-29 22:10:04 -0500

    Update Connect X.py

--- Connect X.py
+++ Connect X.py
@@ -1,6 +1,9 @@
 import PySimpleGUI as sg
 from time import sleep
 
+
+
+#makes sure you have an integer
 def notAnIntTest(number):
     try:
         number = int(number)
@@ -9,20 +12,23 @@ def notAnIntTest(number):
         return None
     return  number
 
+
+
+#checks for win conditions
 def checkForWin(gameBoard,playerTurn, winLength):
     
-    
-        
-    
+
     rows = len(gameBoard)
     columns = len(gameBoard)
+    #number of connected pieces needed for a win
     lengthOfWin = int(winLength)
     count = 0
     maxLen = 0
 
-    
+    #
     #horizontal check
-
+    #
+    
     #for each row
     for i in range(0,rows):
         count = 0
@@ -30,27 +36,37 @@ def checkForWin(gameBoard,playerTurn, winLength):
         for j in range(0,columns):
             if gameBoard[i][j] == playerTurn:
                 count+=1
-        if count >= maxLen:
-            maxLen = count
-        if count >= lengthOfWin:
-            return 1337
+            else:
+                count = 0
+            if count >= maxLen:
+                maxLen = count
+            if count >= lengthOfWin:
+                return 1337
         
     count = 0
     maxLen = 0
 
-    
+    #
     #vertical check
+    #
+    
     for i in range(0,columns):
         count = 0
         for j in range(0,rows):
             if gameBoard[j][i] == playerTurn:
                 count+=1
-        if count >= maxLen:
-            maxLen = count
-        if count >= lengthOfWin:
-            return 1337
+            else:
+                count = 0
+            if count >= maxLen:
+                maxLen = count
 
+            if count >= lengthOfWin:
+                return 1337
+        
+    #
     #diagonal check up right
+    #
+    
     count = 0
     maxLen = 0
     
@@ -62,18 +78,22 @@ def checkForWin(gameBoard,playerTurn, winLength):
             for k in range(0,lengthOfWin):
                 if gameBoard[i-k][j+k] == playerTurn:
                     count+=1
+                else:
+                    count = 0
             
         
-        if count >= maxLen:
-            maxLen = count
-        if count >= lengthOfWin:
-            return 1337
+            if count >= maxLen:
+                maxLen = count
+            if count >= lengthOfWin:
+                return 1337
 
     maxLen = 0
     count = 0
 
-    
+    #
     #diagonal check up left
+    #
+    
     for i in range(rows-1,lengthOfWin-2,-1):
         count = 0
 
@@ -82,28 +102,43 @@ def checkForWin(gameBoard,playerTurn, winLength):
             for k in range(0, lengthOfWin):
                 if gameBoard[i-k][j-k] == playerTurn:
                     count+=1
+                else:
+                    count = 0
 
-            if count >= maxLen:
-                maxLen = count
-            if count >= lengthOfWin:
-                return 1337
-            
+                if count >= maxLen:
+                    maxLen = count
+                if count >= lengthOfWin:
+                    return 1337
+                
+def findEmptySlot(gameBoard, slot):
+    for i in range (0,len(gameBoard)):
+        if gameBoard[slot][i] == 0:
+            continue
+        else:
+            if (i+1 != len(gameBoard)-1 and gameBoard[slot][i+1] != 0):
+                break
+        
+    print("i is "+str(i))
+    return i
        
 
 
-        
+# initialize the boardgame        
 def makeBoard(size):
-    
+    # place a white square for unused squares
     image = 'blank.png'
-    board = [ [0 for j in range(size)] for i in range (size)] 
+    # stores what the game outline looks like
+    board = [ [0 for j in range(size)] for i in range (size)]
     layout = [ [sg.Button( image_filename=image,key = (i,j),pad=(0,0),size=(4,2) ) for j in range (size)] for i in range (size) ]
     playBoard = sg.Window("Connect X",layout,keep_on_top=True).Finalize()
-    playBoard.Maximize()
+    # stores what pieces are supposed to be where
     gameBoard = [ [0]*size for i in range(size)]
     return playBoard,gameBoard
 
 
-def getInput(playBoard,gameBoard, playerTurn, winLength):
+def getInputTTT(playBoard,gameBoard, playerTurn, winLength):
+
+    # player's pieces
     if playerTurn == 1:
         image = 'p1.png'
     elif playerTurn == 2:
@@ -113,17 +148,20 @@ def getInput(playBoard,gameBoard, playerTurn, winLength):
         playBoard.BringToFront()
         event,values = playBoard.read()
         playBoard.BringToFront()
+        #if the player hit the X button, exit the game
         if event == None:
             quit()
+        #if you try to place a piece on an occupied spot
         if gameBoard[event[0]][event[1]] != 0:
             playBoard.hide()
             sg.popup_timed("That's occupied, you dolt", no_titlebar = True, auto_close_duration=5,keep_on_top=True)
             playBoard.UnHide()
             continue
+        #put the piece in if valid
         else:
             playBoard[event].update(image_filename=image)
             gameBoard[event[0]][event[1]]=playerTurn
-
+        
         playBoard.refresh()
         a = checkForWin(gameBoard,playerTurn, winLength)
         if a == 1337:
@@ -135,8 +173,54 @@ def getInput(playBoard,gameBoard, playerTurn, winLength):
             quit()     
         break
         
+def getInputC4(playBoard,gameBoard, playerTurn, winLength):
     
-def gameLoop(playBoard, gameBoard, playerTurn, winLength):
+    # player's pieces
+    if playerTurn == 1:
+        image = 'p1.png'
+    elif playerTurn == 2:
+        image = 'p2.png'
+    
+    while True:
+        playBoard.BringToFront()
+        event,values = playBoard.read()
+        playBoard.BringToFront()
+        #if the player hit the X button, exit the game
+        if event == None:
+            quit()
+
+            
+        #check to see if column is empty
+        #if you try to place a piece on an occupied spot
+        if gameBoard[0][event[1]]!=0:
+            playBoard.hide()
+            sg.popup("That column is occupied, you dolt!")
+            playBoard.UnHide()
+            continue
+
+        
+        #put the piece in if valid
+        else:
+            emptySlot = findEmptySlot(gameBoard,event[1])
+            print(type(emptySlot))
+            print(f"Gonna try for {emptySlot},{event[1]}")
+            #playBoard[(event[emptySlot],event[1])].update(image_filename=image)
+            playBoard[(emptySlot,event[1])].update(image_filename=image)
+            gameBoard[emptySlot][event[1]]=playerTurn
+        
+        playBoard.refresh()
+        a = checkForWin(gameBoard,playerTurn, winLength)
+        if a == 1337:
+            playBoard.hide()
+            sg.popup(f"PLAYER {playerTurn} WINS")
+            playBoard.UnHide()
+            sleep(2)
+            playBoard.close()
+            quit()     
+        break
+
+
+def gameLoop(playBoard, gameBoard, playerTurn, winLength, gameType):
     while True:
         if playerTurn == 1:
             playerTurn = 2
@@ -145,16 +229,24 @@ def gameLoop(playBoard, gameBoard, playerTurn, winLength):
         else:
             sg.popup("Error")
             quit()
-        getInput(playBoard, gameBoard, playerTurn, winLength)
+
+        
+        if gameType == 1:
+            getInputC4(playBoard, gameBoard, playerTurn, winLength)
+        if gameType == 2:
+            getInputTTT(playBoard, gameBoard, playerTurn, winLength)
     
 
+
+
 while True:
-    gameType = sg.popup_get_text("Do you want to play tic tac toe style or connect 4? \n1. Connect 4\n2. Tic Tac Toe  [doesn't matter what you pick in this version]",keep_on_top=True)
+    gameType = sg.popup_get_text("Do you want to play tic tac toe style or connect 4? \n1. Connect 4\n2. Tic Tac Toe",keep_on_top=True)
     returnValue = notAnIntTest(gameType)
     if returnValue != None:
-        boardSize = returnValue
+        gameType = returnValue
     else:
         continue
+    
     
     boardSize = sg.popup_get_text("What's the length of the playing field? (minimum 3, maximum 30)",keep_on_top=True)
     returnValue = notAnIntTest(boardSize)
@@ -178,5 +270,7 @@ while True:
     
     
     playBoard,gameBoard = makeBoard(boardSize)
-    gameLoop(playBoard, gameBoard, 2, winLength)
+
+
+    gameLoop(playBoard, gameBoard, 2, winLength, gameType)
     
